1c4bf940234fbcf6844e5065edc16e7ab9fb67d6
Ernesto Roldan <Ernesto.Roldan.Rh@gmail.com>
2018-03-01 23:51:33 -0600

    fixing error wwcd

--- bot.js
+++ bot.js
@@ -774,10 +774,12 @@ for (var z = 0; z < commandArray.length; z++)
         console.log('rand value'+(getRandomInt(row.id)+1));
 
 
-        db.get("SELECT candyQuotes FROM wwcd WHERE rowid ="+(getRandomInt(row.id)+1),function(err, row){
+        db.get("SELECT candyQuotes FROM wwcd WHERE rowid ="+(getRandomInt(row.id)+1),function(err, row1){
 
         
-                message.reply(row.quote);
+                message.reply(row1.quote);
+
+                console.log('value of the quote'+row1.quote);
         });
 
 
